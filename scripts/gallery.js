5a10e049c827eaf59d68ad17219b56b837d0c519
ci0cc0lat0 <anthonymciocco@gmail.com>
2022-05-07 03:36:25 -0500

    Fixed List, pretty-ed code

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,107 +1,107 @@
 const galleryList = [
-    {
-      'name':'angst.png',
-      'tag':'sfw'
-    },
-    {
-      'name':'Classic Cutie.png',
-      'tag':'sfw'
-    },
-    {
-      'name':'SomePractice.png',
-      'tag':'sfw'
-    },
-    {
-      'name':'MoreMyTake.png',
-      'tag':'sfw'
-    },
-    {
-      'name':'ThisIsntAboutYou.png',
-      'tag':'sfw'
-    },
-    {
-      'name': 'baddays.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'FuckIfIKnow.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'CebCock.png',
-      'tag': 'nsfw'
-    },
-    {
-      'name': 'CursedGrabNewBooba.png',
-      'tag': 'nsfw'
-    },
-    {
-      'name': 'How i hold my joystick.png',
-      'tag': 'nsfw'
-    },
-    {
-      'name': 'Pasted image 20211118231633.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'Pasted image 20211118231830.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'Pasted image 20211118231914.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'Right My Wrongs.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'Scene.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'SpaceCowBoy.png',
-      'tag': 'nsfw'
-    },
-    {
-      'name': 'ash.png',
-      'tag': 'sfw'
-    },
-  
-    {
-      'name': 'beachnobody.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'maggie.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'midrift.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'no3.jpg',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'pepe.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'pl.jpg',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'someboxes.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'waiting.png',
-      'tag': 'sfw'
-    },
-    {
-      'name': 'why.png',
-      'tag': 'sfw'
-    }
-  ]
+  {
+    'name': 'angst.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'Classic Cutie.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'SomePractice.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'MoreMyTake.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'ThisIsntAboutYou.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'baddays.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'FuckIfIKnow.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'CebCock.png',
+    'tag': 'nsfw'
+  },
+  {
+    'name': 'CursedGrabNewBooba.png',
+    'tag': 'nsfw'
+  },
+  {
+    'name': 'How i hold my joystick.png',
+    'tag': 'nsfw'
+  },
+  {
+    'name': 'Pasted image 20211118231633.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'Pasted image 20211118231830.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'Pasted image 20211118231914.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'Right My Wrongs.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'Scene.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'SpaceCowBoy.png',
+    'tag': 'nsfw'
+  },
+  {
+    'name': 'ash.png',
+    'tag': 'sfw'
+  },
+
+  {
+    'name': 'beachnobody.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'maggie.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'midrift.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'no3.jpg',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'pepe.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'pl.jpg',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'someboxes.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'waiting.png',
+    'tag': 'sfw'
+  },
+  {
+    'name': 'why.png',
+    'tag': 'sfw'
+  }
+]
--- scripts/list.js
+++ scripts/list.js
@@ -1,118 +1,118 @@
 const myListArray = [
     {
-        name:'Values'
-        caption:'tk'
+        name: 'Values',
+        caption: 'Live and let live<br>Be the bigger person<br>Treat others as ends<br>Be fair<br>Have understanding<br>Moderation is a virtue<br>Know oneself'
     },
     {
-        name:'Turmoil',
-        caption:'tk'
+        name: 'Turmoil',
+        caption: 'tk'
     },
     {
-        name:"Happy for you",
-        caption:"I see <b>you</b>, and I am happy for <b>you</b>. I see how well <b>you</b> are doing, how happy <b>you</b> are, and what <b>you</b> deem important is still by <b>you</b>r side. <b>You</b> worked hard to get where <b>you</b> are. For my friends, acquantances, and anyone inbetween I wish <b>you</b> the best. <b>You</b> all deserve it. Best wishes."
-    },
-  {
-    name:"The Attention",
-    caption:"Back then, I was living life like a ghost, thought not the host I’m about to tell you about. I was all numb and stupid and dazed too I guess, a pretty spooky silentiary for matters I knew by heart but could never quite translate for anyone I knew let alone myself. I constantly, craved the comforts of feminine attention, even though the thought of actually getting a girlfriend, one who was into me and wanted to be with me, seemed about as real as any dozen of the myth’s I’d been reading about in class"
-  },
-  {
-    name:"Ghost",
-    caption:"I see them all to often. Those who were tried and failed, those who were cared for and ignored, efforts put in and thanks spit out. I get irrational around them and I feel as if they act irrationally around me. While I am stuck in this plane, they wonder theirs without a second thought. Perhaps that is the hint; Continue without giving them a thought."
-  },
-  {
-      name:"Means to an end",
-      caption:"While the reason might be benign, the intent feels unjust. The intent feels slimey. Disingenuous. In that moment, that tiny reason is the cause. For any moments. All for my own sake. All for my own praise, desires, and needs. If I had a different outlook, a different set of values, a changed world view, maybe these actions could be justified.<br> I dont want to feel this way forever"
+        name: "Happy for you",
+        caption: "I see <b>you</b>, and I am happy for <b>you</b>. I see how well <b>you</b> are doing, how happy <b>you</b> are, and what <b>you</b> deem important is still by <b>you</b>r side. <b>You</b> worked hard to get where <b>you</b> are. For my friends, acquantances, and anyone inbetween I wish <b>you</b> the best. <b>You</b> all deserve it. Best wishes."
     },
     {
-        name:"Where are you now?",
-        caption:"I look at others from my past and wonder this. So many people I want to catch up with but nothing but ghost and misplaced meaning wait there"
+        name: "The Attention",
+        caption: "Back then, I was living life like a ghost, thought not the host I’m about to tell you about. I was all numb and stupid and dazed too I guess, a pretty spooky silentiary for matters I knew by heart but could never quite translate for anyone I knew let alone myself. I constantly, craved the comforts of feminine attention, even though the thought of actually getting a girlfriend, one who was into me and wanted to be with me, seemed about as real as any dozen of the myth’s I’d been reading about in class"
     },
     {
-        name:"Apathy be the death of me",
-        caption:"Taken from TheActMan about video games, when I self reflect, I feel that phrase can be used to describe me. A lot of my life I have been apathetic to how I approach things. Has it landed me in a terrible spot? No. Will it evenutally? probably."
+        name: "Ghost",
+        caption: "I see them all to often. Those who were tried and failed, those who were cared for and ignored, efforts put in and thanks spit out. I get irrational around them and I feel as if they act irrationally around me. While I am stuck in this plane, they wonder theirs without a second thought. Perhaps that is the hint; Continue without giving them a thought."
     },
     {
-        name:"Inspiration",
-        caption:"It was then I had inspiration. Misguided and nonsensical, something about 'it' left me wanting. I can work towards it. I can achieve it. In the grand scheme of things, it is rather silly."
+        name: "Means to an end",
+        caption: "While the reason might be benign, the intent feels unjust. The intent feels slimey. Disingenuous. In that moment, that tiny reason is the cause. For any moments. All for my own sake. All for my own praise, desires, and needs. If I had a different outlook, a different set of values, a changed world view, maybe these actions could be justified.<br> I dont want to feel this way forever"
     },
     {
-                name:"Why Do I Hesitate",
-                caption:"tk"
-        },
+        name: "Where are you now?",
+        caption: "I look at others from my past and wonder this. So many people I want to catch up with but nothing but ghost and misplaced meaning wait there"
+    },
     {
-                name:"The feeling is mutual",
-                caption:"tk"
-        },
+        name: "Apathy be the death of me",
+        caption: "Taken from TheActMan about video games, when I self reflect, I feel that phrase can be used to describe me. A lot of my life I have been apathetic to how I approach things. Has it landed me in a terrible spot? No. Will it evenutally? probably."
+    },
     {
-                name:"...---...",
-                caption:"tk"
-        },
+        name: "Inspiration",
+        caption: "It was then I had inspiration. Misguided and nonsensical, something about 'it' left me wanting. I can work towards it. I can achieve it. In the grand scheme of things, it is rather silly."
+    },
     {
-                name:"Melanhap",
-                caption:"tk"
-        },
+        name: "Why Do I Hesitate",
+        caption: "tk"
+    },
     {
-                name:"Contrived smiles and a bill of goods",
-                caption:"tk"
-        },
+        name: "The feeling is mutual",
+        caption: "tk"
+    },
     {
-                name:"Unreliable narrator",
-                caption:"tk"
-        },
+        name: "...---...",
+        caption: "tk"
+    },
     {
-                name:"She likes to talk",
-                caption:"Funnily enough, she could talk that talk, but couldn't walk that walk. People <strike>rarely</strike> neglect to ask about others"
-        },
+        name: "Melanhap",
+        caption: "tk"
+    },
     {
-                name:"I'm not sure what I'm looking for",
-                caption:"tk"
-        },
+        name: "Contrived smiles and a bill of goods",
+        caption: "tk"
+    },
     {
-                name:"Put an * next to my name",
-                caption:"tk"
-        },
+        name: "Unreliable narrator",
+        caption: "tk"
+    },
     {
-                name:"White Lies",
-                caption:"tk"
-        },
+        name: "She likes to talk",
+        caption: "Funnily enough, she could talk that talk, but couldn't walk that walk. People <strike>rarely</strike> neglect to ask about others"
+    },
     {
-                name:"Imaginary Muse",
-                caption:"tk"
-        },
+        name: "I'm not sure what I'm looking for",
+        caption: "tk"
+    },
     {
-                name:'Ashley\'s safety',
-                caption:"tk"
-        },
+        name: "Put an * next to my name",
+        caption: "tk"
+    },
     {
-                name:'What if wonders',
-                caption:"tk"
-        },
+        name: "White Lies",
+        caption: "tk"
+    },
     {
-                name:'Don\'t overdose on happiness',
-                caption:"tk"
-        },
+        name: "Imaginary Muse",
+        caption: "tk"
+    },
     {
-                name:'Depression',
-                caption:"tk"
-        },
+        name: 'Ashley\'s safety',
+        caption: "tk"
+    },
     {
-                name:'You\'re one to talk',
-                caption:"tk"
-        },
+        name: 'What if wonders',
+        caption: "tk"
+    },
     {
-                name:'Someone to talk to',
-                caption:"We all look for that person. Often some are lucky enough to find that person. They stay, they care, they listen. Other times they are present, yet invisible, noticed but silent. Indeed it is a <strike>thankless job.</strike>"
-        },
+        name: 'Don\'t overdose on happiness',
+        caption: "tk"
+    },
     {
-                name:'I am not sure why we have to keep doing this shit',
-                caption:"tk"
-        },
+        name: 'Depression',
+        caption: "tk"
+    },
     {
-                name:'I dont know enough',
-                caption:"tk"
-        },
+        name: 'You\'re one to talk',
+        caption: "tk"
+    },
     {
-                name:"The less I know",
-                caption:"tk"
-        }
+        name: 'Someone to talk to',
+        caption: "We all look for that person. Often some are lucky enough to find that person. They stay, they care, they listen. Other times they are present, yet invisible, noticed but silent. Indeed it is a <strike>thankless job.</strike>"
+    },
+    {
+        name: 'I am not sure why we have to keep doing this shit',
+        caption: "tk"
+    },
+    {
+        name: 'I dont know enough',
+        caption: "tk"
+    },
+    {
+        name: "The less I know",
+        caption: "tk"
+    }
 ]
